21cc0506d461c9178cf7df4e22b1b93a3812ed35
Robert <rquinones93@yahoo.com>
2018-06-20 20:57:45 -0700

    Reformatted server.js using Beautify

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require( 'express' );
-const mongoose = require( 'mongoose' );
+const express = require('express');
+const mongoose = require('mongoose');
 
 // Route Setup
-const users = require( './routes/api/users' );
+const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
@@ -10,24 +10,24 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Database Configuration
-const database = require( './config/keys' ).mongoURI;
+const database = require('./config/keys').mongoURI;
 
 // Connect to mLab MongoDB
-mongoose.connect( database )
-  .then( () => {
-    console.log( 'MongoDB Connected' );
+mongoose.connect(database)
+  .then(() => {
+    console.log('MongoDB Connected');
 
-  }).catch( error => console.log( error ) );
+  }).catch(error => console.log(error));
 
 // Route Usage
-app.use( '/api/users', users );
-app.use('/api/profile', profile );
-app.use('/api/posts', posts );
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 
-  // Change port depending on deployment vs local
+// Change port depending on deployment vs local
 const port = process.env.PORT || 5000;
 
-app.listen( port, () => {
-  console.log( `Server running on port ${port}` );
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
