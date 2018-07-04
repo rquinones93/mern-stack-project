b6b289bbccfe0a2169762b578a12198236ed324c
Robert <rquinones93@yahoo.com>
2018-07-04 01:36:22 -0700

    Finished Current User Profile Route portion of course

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,34 @@
 // Helps create and update the User's Profile
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose'); // Connection to DB
+const passport = require('passport'); // Protected routes
 
-router.get('/test', (request, response) => {
-  response.json({
-    msg: 'Profile Works'
-  });
+// Load Profile Model
+const Profile = require('../../models/Profile');
+
+// Load User Profile 
+const User = require('../../models/Users');
+
+// @route GET api/profile
+// @desc  Get current user's profile
+// @access Private - use jwt strategy to authenticatte
+router.get('/', passport.authenticate('jwt', {session: false}), (request, response) => {
+  const errors = {};
+
+  // Check profile table to find user based on their id
+  Profile.findOne({user: request.user.id})
+    .then(profile => {
+      // No valid user profile related to id
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return response.status(404).json(errors);
+      }
+
+      // Valid user profile related to id
+      response.json(profile);
+    })
+    .catch(error => response.status(404).json(error));
 });
 
 module.exports = router;
